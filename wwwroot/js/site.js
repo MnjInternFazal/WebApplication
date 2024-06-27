6b4f91b5e9bcfb688a47eedf66f31753af163efb
MnjInternFazal <165366068+MnjInternFazal@users.noreply.github.com>
2024-06-27 15:23:49 +0530

    Add files via upload

---
+++ wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+document.addEventListener('DOMContentLoaded', function () {
+    var menuIcon = document.getElementById('menu-icon');
+    var sidebar = document.getElementById('sidebar');
+
+    menuIcon.addEventListener('click', function () {
+        sidebar.classList.toggle('open');
+    });
+});
